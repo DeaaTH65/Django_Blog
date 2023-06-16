13a55b07e2cee86f34b85da6923cb61521602239
Deaath65 <deaath65@gmail.com>
2023-06-16 07:11:31 +0545

    PG connected

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -64,10 +64,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'Dblog',
+       'USER': 'postgres',
+       'PASSWORD': 'Deaath@65',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
